e062e4e99b89838c9829ccc070bc80388cbadb96
advanze <felipe@agile.cl>
2022-07-09 22:56:37 -0400

    Refactor to Exercise1.5

--- src/main/java/io/workshop/reactive/Exercise1.java
+++ src/main/java/io/workshop/reactive/Exercise1.java
@@ -11,14 +11,20 @@ public class Exercise1 {
     public static void main(String[] args) {
 
         // TODO: PRINT ALL STRING and INT NUMBERS
-        StreamSources.bringNumbersAsString().forEach(out::println);
-        StreamSources.bringNumbersAsInteger().forEach(out::println);
+        StreamSources.bringNumbersAsString()
+                .forEach(out::println);
+
+        StreamSources.bringNumbersAsInteger()
+                .forEach(out::println);
 
         // TODO: PRINT ALL NUMBERS THAT ARE LESS THAN 5
-        StreamSources.bringNumbersAsInteger().filter(num -> num < 5).forEach(out::println);
+        StreamSources.bringNumbersAsInteger()
+                .filter(num -> num < 5)
+                .forEach(out::println);
 
         // TODO: PRINT THE SECOND AND THIRD NUMBER THATS GREATER THAN 5
-        StreamSources.bringNumbersAsInteger().filter(num -> num > 5)
+        StreamSources.bringNumbersAsInteger()
+                .filter(num -> num > 5)
                 .skip(1)
                 .limit(2)
                 .forEach(out::println);
@@ -29,19 +35,21 @@ public class Exercise1 {
                 .findFirst()
                 .ifPresentOrElse(val -> {
                    out.println(String.valueOf(val));
-                }, () ->{
+                }, () -> {
                     out.println("-1");
                 });
 
         // TODO: Print first names of all users in userStream
-        StreamSources.bringUsers().forEach(user -> {
-            String[] splittedName = user.getName().split(" ");
-            out.println(splittedName[0]);
-        });
+        StreamSources.bringUsers()
+                .forEach(user -> {
+                    Arrays.stream(user.getName().split(" "))
+                    .findFirst()
+                    .ifPresent(out::println);
+                });
 
         // TODO: Print first names in userStream for users that have IDs from number stream
         StreamSources.bringUsers()
-                // Filter users which ID is not contained in Stream of IDS
+                // Filter users which ID is not contained in Stream of IDS. Then pass the users filtered to a list
                 .filter(
                         user -> StreamSources.bringNumbersAsInteger()
                                 .toList()
